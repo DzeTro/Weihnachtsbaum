e786248c920d52abeb846b7e36adb8e909fdd294
Dzevad Trokic <d.trokic@cp-pro.de>
2022-03-28 08:41:12 +0200

    Projektdateien hinzufügen.

---
+++ NAdelbaum 2/Program.cs
@@ -0,0 +1,55 @@
+﻿
+Console.WriteLine("               *                 ", Console.ForegroundColor = ConsoleColor.DarkGreen);
+Console.WriteLine("              ***                ", Console.ForegroundColor = ConsoleColor.DarkGreen);
+Console.WriteLine("             *****               ", Console.ForegroundColor = ConsoleColor.DarkGreen);
+
+
+int numb = 0;
+
+Console.Write("Höhe (Reihen) = ");
+numb = int.Parse(Console.ReadLine());
+
+
+Nadelbaum(numb);
+
+
+static void Nadelbaum(int reihen)
+{
+    if (reihen < 3)
+    {
+        reihen = 3;
+    }
+
+    int maxStamm = reihen / 15;
+    int tempStamm = 0;
+
+    //Krone
+    for (int i = 0; i <= reihen; i++)
+    {
+        for (int j = i; j <= reihen; j++)
+        {
+            Console.Write(" ");
+        }
+        for (int k = 0; k < i; k++)
+        {
+            Console.Write("* ");
+        }
+        Console.WriteLine();
+    }
+
+    //Stamm
+    do
+    {
+        for (int j = 0; j < reihen - (reihen / 3); j++)
+        {
+            Console.Write(" ");
+        }
+        for (int k = 1; k <= reihen / 3; k++)
+        {
+            Console.Write(" #");
+        }
+        Console.WriteLine();
+        tempStamm++;
+
+    } while (tempStamm <= maxStamm);
+}
